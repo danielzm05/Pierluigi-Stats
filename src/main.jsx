cc29468e651dea6e212e52a41b8d5231938d7787
danielzm05 <danielzmfcbarcelona@gmail.com>
2024-06-08 16:03:08 -0300

    I added React Router library to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { FiltersProvider } from './context/filter.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import { FiltersProvider } from "./context/filter.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <FiltersProvider>
-    <App />
-  </FiltersProvider>
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/pierluigi-stats" element={<Home />} />
+      <Route
+        path="/pierluigi-stats/dashboard"
+        element={
+          <FiltersProvider>
+            <App />
+          </FiltersProvider>
+        }
+      />
+    </Routes>
+  </Router>
+);
